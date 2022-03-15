f5476b397a7b9cf9ec8b3b5a14ccae6d443d05e2
dealencarmarcelo <dealencarmarcelo@gmail.com>
2022-03-14 22:50:09 -0300

    feat: add events validations - specs successful

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,18 @@ class Event < ApplicationRecord
   has_many :users_events
   has_many :users, through: :users_events
   has_many :results
+
+  validates_presence_of :name, :start_date, :end_date
+
+  validate :verify_start_date
+
+  private
+
+  def verify_start_date
+    return false unless start_date && end_date
+
+    errors.add(:start_date, 'must be before end_date') unless start_date.before?(end_date)
+
+    false
+  end
 end
