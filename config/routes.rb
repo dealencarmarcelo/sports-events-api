d945bfc111a2bedf6ff3055100642bdaa8d9e1b0
dealencarmarcelo <dealencarmarcelo@gmail.com>
2022-03-18 01:54:00 -0300

    fix: routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :events do
-        resources :results
+        resources :results, only: %w[index show create]
 
         member do
           post 'subscribe'
           get 'competitors'
-        end
-
-        member do
           get :ranking
         end
       end
@@ -20,8 +17,6 @@ Rails.application.routes.draw do
           post :signup
         end
       end
-
-      resources :users
     end
   end
 end
